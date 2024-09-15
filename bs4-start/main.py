2946fffc2981e01726d402cd14f34e91bd937860
AkaashSamson <akaashsam22@gmail.com>
2024-09-15 18:36:26 +0530

    find, findall and select

--- bs4-start/main.py
+++ bs4-start/main.py
@@ -5,9 +5,29 @@ with open("D:/Python_practs/Web-Scraping/bs4-start/website.html") as file:
 
 soup = BeautifulSoup(contents, 'html.parser')
 
+all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
+
+for tag in all_anchor_tags:
+    #print(tag.getText())
+    print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+print(heading)
 print()
-print(soup.title)
+
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading)
 print()
-print(soup.title.string)
+
+company_url = soup.select_one(selector="p a")
+print(company_url)
 print()
-print(soup.prettify())
+
+name = soup.select_one("#name")
+print(name)
+print()
+
+headings = soup.select(".heading")
+print(headings)
+print()
