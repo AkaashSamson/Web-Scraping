246653bb249de4bd27791f2302f2b7912cc9d4ea
AkaashSamson <akaashsam22@gmail.com>
2024-09-28 07:41:59 +0530

    Sort in order of scores

--- bs4-start/main.py
+++ bs4-start/main.py
@@ -14,8 +14,16 @@ soup = BeautifulSoup(yc_webpage, 'html.parser')
 atags = soup.find_all('span', class_='titleline')
 
 article_score = soup.find_all(name='span', class_='score')
-
+proper_list = []
 for idx,tag,sc in zip(range(0, len(atags)), atags, article_score):
     title = tag.find(name='a').getText()
     link = tag.find(name='a').get('href')
-    print(f"{idx+1}: {title} ({sc.getText()}) - {link}")
+    score = int(sc.getText().split()[0])
+    # print(f"{idx+1}: {title} ({sc.getText().split()[0]}) - {link}")
+    proper_list.append({'title': title, 'link': link, 'score': score})
+
+#Sort the list of dictionaries by score
+sorted_list = sorted(proper_list, key=lambda k: k['score'], reverse=True)
+
+for idx, item in enumerate(sorted_list):
+    print(f"{idx+1}: {item['title']} ({item['score']}) - {item['link']}")
